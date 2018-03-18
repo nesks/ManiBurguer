465216c821fc759fca36a7691d9874680b0d2151
indigente <neskssousa@gmail.com>
2018-03-17 21:18:29 -0300

    carrinho criado, falta adicionar preço de cada hamb e preço geral

--- app/src/main/java/com/maniburguer/hamburgueria/maniburguer/Fragment/EnviarFragment.java
+++ app/src/main/java/com/maniburguer/hamburgueria/maniburguer/Fragment/EnviarFragment.java
@@ -30,7 +30,6 @@ public class EnviarFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
